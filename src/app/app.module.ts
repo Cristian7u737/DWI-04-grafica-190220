3488ec700fbc003f995630808f6591e660d2ceab
Cristian7w7 <nochebuenacristian048@gmail.com>
2022-08-02 16:25:54 -0500

    Gráfica dinámica - Sin Sockets

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { NgChartsModule } from 'ng2-charts';
+import { GraficaComponent } from './components/grafica/grafica.component';
+
+const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
+@NgModule({
+  declarations: [
+    AppComponent,
+    GraficaComponent
+  ],
+  imports: [
+    BrowserModule,
+    SocketIoModule.forRoot(config),
+    NgChartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
